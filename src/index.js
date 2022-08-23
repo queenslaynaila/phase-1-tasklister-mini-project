7996b3555ffd5a28a69bf47d2c5be667cdb9f05e
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-23 08:05:23 +0300

    Done assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const input = document.getElementById("create-task-form");
+  const formElement = document.querySelector("form");
+  formElement.addEventListener("submit", (e) => {
+    let newtask = input.value;
+
+    e.preventDefault();
+
+    handleToDo(e.target.new_todo.value);
+    formElement.reset();
+  });
 });
+
+function handleToDo(toDo) {
+  console.log(toDo);
+  let p = document.createElement("p");
+  let btn = document.createElement("button");
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "x";
+  p.textContent = `${toDo} `;
+  p.appendChild(btn);
+  console.log(p);
+  document.querySelector("#list").appendChild(p);
+}
+function handleDelete(e) {
+  e.target.parentNode.remove;
+}
